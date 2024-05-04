80eac99f11e736cafe815c668f799d149c7d3da6
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2024-05-04 16:23:15 +0000

    update

--- system/cpu.go
+++ system/cpu.go
@@ -139,8 +139,7 @@ func getCpuInfo(ret *model.SystemInfo, cpuType string) (*model.SystemInfo, error
 			}
 		}
 	}
-	// 使用 sysctl 获取信息 - 特化适配 freebsd openbsd 系统
-
+	// TODO 使用 sysctl 获取信息 - 特化适配 freebsd openbsd 系统
 	// 使用 /proc/device-tree 获取信息 - 特化适配嵌入式系统
 	deviceTreeContent, err := os.ReadFile("/proc/device-tree")
 	if err == nil {

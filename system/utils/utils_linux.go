81213bfb6b2009c421afc7afdb645cb6484862e5
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2024-05-04 11:40:25 +0000

    update

--- system/utils/utils_linux.go
+++ system/utils/utils_linux.go
@@ -3,7 +3,7 @@ package utils
 import (
 	"bytes"
 	"os/exec"
-	"time"
+	"strings"
 )
 
 // GetCpuCache 查询CPU三缓
@@ -36,9 +36,35 @@ func GetTCPAccelerateStatus() string {
 	}
 }
 
+// 解析时区信息
+func parseTimeZone(output string) string {
+	// 在输出中查找 Time zone 字符串
+	index := strings.Index(output, "Time zone")
+	if index != -1 {
+		// 如果找到，则截取 Time zone 字符串后的部分
+		output = output[index+len("Time zone")+1:]
+		// 找到冒号后的第一个空格，分割字符串获取时区信息
+		parts := strings.SplitN(output, " ", 2)
+		if len(parts) > 1 {
+			return strings.TrimSpace(parts[1])
+		}
+	}
+	return ""
+}
+
 // GetTimeZone 获取当前时区
 func GetTimeZone() string {
-	local := time.Now().Location()
-	CurrentTimeZone := local.String()
+	var CurrentTimeZone string
+	output, err := exec.Command("timedatectl", "|", "grep", "Time zone").Output()
+	if err == nil && strings.Contains(string(output), "Time zone") {
+		timeZone := parseTimeZone(string(output))
+		CurrentTimeZone = timeZone
+	} else {
+		output, err = exec.Command("date", "+%Z").Output()
+		if err == nil {
+			timeZone := strings.TrimSpace(string(output))
+			CurrentTimeZone = timeZone
+		}
+	}
 	return CurrentTimeZone
 }

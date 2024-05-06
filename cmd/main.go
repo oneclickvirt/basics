4d83cb3133db4968d87e72aa5e89f8dc55f022f8
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2024-05-06 08:28:58 +0000

    update

--- cmd/main.go
+++ cmd/main.go
@@ -13,9 +13,9 @@ func main() {
 		http.Get("https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2Foneclickvirt%2Fbasics&count_bg=%232EFFF8&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false")
 	}()
 	fmt.Println("项目地址:", "https://github.com/oneclickvirt/gostun")
+	ipInfo, _, _ := network.NetworkCheck("both", false, "zh")
 	fmt.Println("--------------------------------------------------")
 	system.CheckSystemInfo()
-	ipInfo, _, _ := network.NetworkCheck("both", false, "zh")
 	fmt.Printf(ipInfo)
 	fmt.Println("--------------------------------------------------")
 }

3c03cf668db802845b30af9a95c151a8889dae49
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2025-07-27 23:55:29 +0800

    fix: 修复错误的打印方式

--- network/network_test.go
+++ network/network_test.go
@@ -10,6 +10,6 @@ func TestIpv4SecurityCheck(t *testing.T) {
 	// 全项测试
 	ipInfo, _, _ := NetworkCheck("both", false, "zh")
 	fmt.Println("--------------------------------------------------")
-	fmt.Printf(ipInfo)
+	fmt.Print(ipInfo)
 	fmt.Println("--------------------------------------------------")
 }
